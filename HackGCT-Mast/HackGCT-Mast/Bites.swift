17e1f099769e3c0f946241ae851f34c0ce8a2cae
Zachary Espiritu <zespiritu17@regis.org>
2015-08-08 14:40:37 -0400

    Test

--- HackGCT-Mast/HackGCT-Mast/Bites.swift
+++ HackGCT-Mast/HackGCT-Mast/Bites.swift
@@ -1,90 +1,90 @@
+////
+////  Bites.swift
+////  HackGCT-Mast
+////
+////  Created by nico cobb on 8/8/15.
+////  Copyright (c) 2015 hackgctmast. All rights reserved.
+////
 //
-//  Bites.swift
-//  HackGCT-Mast
+//import Foundation
 //
-//  Created by nico cobb on 8/8/15.
-//  Copyright (c) 2015 hackgctmast. All rights reserved.
-//
-
-import Foundation
-
-class Bites {
-    
-    // MARK: Singleton
-    
-    class var sharedInstance : DataInterpreter {
-        struct Static {
-            static let instance : DataInterpreter = DataInterpreter()
-        }
-        return Static.instance
-    }
-    
-    // MARK: Real Stuff
-    
-    //    var monthlyDataPlanMB: Int = 4 * 1024
-    //    var maxPerDay: Double = 0
-    //    var maxThisDay: Double = 0
-    //    var carryOver: Double = 0
-    var monthlyDataPlanMB: Int = NSUserDefaults.standardUserDefaults().integerForKey("monthlyDataPlanMB") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(monthlyDataPlanMB, forKey:"monthlyDataPlanMB")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var maxPerDay: Double = NSUserDefaults.standardUserDefaults().integerForKey("maxPerDay") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(maxPerDay, forKey:"maxPerDay")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var maxThisDay: Double = NSUserDefaults.standardUserDefaults().integerForKey("maxThisDay") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(maxThisDay, forKey:"maxThisDay")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var carryOver: Double = NSUserDefaults.standardUserDefaults().integerForKey("carryOver") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(carryOver, forKey:"carryOver")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var previousFeedingBites: Int = NSUserDefaults.standardUserDefaults().integerForKey("previousFeedingBites") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(previousFeedingBites, forKey:"previousFeedingBites")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var currentFeedingBites: Int = NSUserDefaults.standardUserDefaults().integerForKey("currentFeedingBites") {
-        didSet {
-            NSUserDefaults.standardUserDefaults().setInteger(currentFeedingBites, forKey:"currentFeedingBites")
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
-    var dataChecker: DataInterpreter = DataInterpreter()
-    var date = NSDate()
-    var calendar = NSCalendar.currentCalendar()
-    var compontents = NSCalendar.currentCalendar().components
-    
-    func assignMonthlyDataGB(data: Int) {
-        monthlyDataPlanMB = data * 1024
-        maxPerDay = monthlyDataPlanMB / date.description
-    }
-    
-    func endOfDayUpdate() {
-        
-    }
-    
-    func beginningOfDayReset() {
-        
-    }
-    
-    func generalUpdate() {
-        dataChecker.getTotalDataUsage()
-    }
-    
-    func spend() -> Int {
-        
-        return currentFeedingBites
-    }
-}
+//class Bites {
+//    
+//    // MARK: Singleton
+//    
+//    class var sharedInstance : DataInterpreter {
+//        struct Static {
+//            static let instance : DataInterpreter = DataInterpreter()
+//        }
+//        return Static.instance
+//    }
+//    
+//    // MARK: Real Stuff
+//    
+//    //    var monthlyDataPlanMB: Int = 4 * 1024
+//    //    var maxPerDay: Double = 0
+//    //    var maxThisDay: Double = 0
+//    //    var carryOver: Double = 0
+//    var monthlyDataPlanMB: Int = NSUserDefaults.standardUserDefaults().integerForKey("monthlyDataPlanMB") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(monthlyDataPlanMB, forKey:"monthlyDataPlanMB")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var maxPerDay: Double = NSUserDefaults.standardUserDefaults().integerForKey("maxPerDay") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(maxPerDay, forKey:"maxPerDay")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var maxThisDay: Double = NSUserDefaults.standardUserDefaults().integerForKey("maxThisDay") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(maxThisDay, forKey:"maxThisDay")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var carryOver: Double = NSUserDefaults.standardUserDefaults().integerForKey("carryOver") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(carryOver, forKey:"carryOver")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var previousFeedingBites: Int = NSUserDefaults.standardUserDefaults().integerForKey("previousFeedingBites") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(previousFeedingBites, forKey:"previousFeedingBites")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var currentFeedingBites: Int = NSUserDefaults.standardUserDefaults().integerForKey("currentFeedingBites") {
+//        didSet {
+//            NSUserDefaults.standardUserDefaults().setInteger(currentFeedingBites, forKey:"currentFeedingBites")
+//            NSUserDefaults.standardUserDefaults().synchronize()
+//        }
+//    }
+//    var dataChecker: DataInterpreter = DataInterpreter()
+//    var date = NSDate()
+//    var calendar = NSCalendar.currentCalendar()
+//    var compontents = NSCalendar.currentCalendar().components
+//    
+//    func assignMonthlyDataGB(data: Int) {
+//        monthlyDataPlanMB = data * 1024
+//        maxPerDay = monthlyDataPlanMB / date.description
+//    }
+//    
+//    func endOfDayUpdate() {
+//        
+//    }
+//    
+//    func beginningOfDayReset() {
+//        
+//    }
+//    
+//    func generalUpdate() {
+//        dataChecker.getTotalDataUsage()
+//    }
+//    
+//    func spend() -> Int {
+//        
+//        return currentFeedingBites
+//    }
+//}
