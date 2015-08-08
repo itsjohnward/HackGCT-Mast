aea7423e3305c64e790729efa848f783c70fb43d
John Ward <jsward2@gmail.com>
2015-08-08 12:10:35 -0400

    Add UI prototype

--- HackGCT-Mast/HackGCT-Mast/ViewController.swift
+++ HackGCT-Mast/HackGCT-Mast/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var money_saved: UILabel!
+    @IBOutlet var data_progress: UIView!
+    @IBOutlet weak var progress: UIProgressView!
+    var dataLimit = 4.0;
+    var data = 0.0;
+    @IBOutlet weak var data_label: UILabel!
+    
+    @IBAction func button(sender: AnyObject) {
+        //progressLabel.text = "0%"
+        data++;
+        data_label.text = data.description + "/" + dataLimit.description;
+        progress.setProgress((Float)(data/dataLimit), animated: true);
+        if (data > dataLimit) {
+            progress.progressTintColor = UIColor.redColor();
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        progress.setProgress(0.0, animated: true);
     }
 
     override func didReceiveMemoryWarning() {
